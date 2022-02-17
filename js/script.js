9dafe96c143fc00e34bdda50c83514f4782f034c
fahimrimon <fahimrimon21@gmail.com>
2022-02-17 13:36:10 +0600

    complete calculation

--- js/script.js
+++ js/script.js
@@ -23,12 +23,19 @@ document.getElementById('calculate-button').addEventListener('click', function (
   // Total expenses
   const totalExpenses = document.getElementById('total-expenses');
   const totalExpensesText = parseFloat(totalExpenses.innerText);
-  const totalExpensesAmount = totalExpenses.innerText = parseFloat(foodAmount + rentAmount + clothesAmount);
+  totalExpenses.innerText = parseFloat(foodAmount + rentAmount + clothesAmount);
+  const totalExpensesAmount = totalExpenses.innerText;
 
   // balance innertext section
   const balance = document.getElementById('balance');
   const balanceText = parseFloat(balance.innerText);
   balance.innerText = parseFloat(incomeAmount - totalExpensesAmount);
+  const balanceAmount = balance.innerText;
+
+  // Remaining Balance section -1 
+  const remainingBalance = document.getElementById('remaining-balance');
+  const remainingBalanceText = parseFloat(remainingBalance.innerText);
+  remainingBalance.innerText = balanceAmount;
 })
 
 // Saving Section 
@@ -36,5 +43,17 @@ document.getElementById('save-button').addEventListener('click', function () {
   const saveInput = document.getElementById('save-input');
   const saveAmount = parseFloat(saveInput.value);
   saveInput.value = '';
+  // Saving Amount section 
+  const saving = document.getElementById('saving-amount');
+  const savingText = parseFloat(saving.innerText);
+  const balance = document.getElementById('balance');
+  const balanceAmount = balance.innerText;
+  saving.innerText = parseFloat((saveAmount / 100) * balanceAmount);
+  const savingAmount = saving.innerText;
+
+  // Remaining Balance section -2 
+  const remainingBalance = document.getElementById('remaining-balance');
+  remainingBalance.innerText = parseFloat(balanceAmount - savingAmount);
+
 })
 
